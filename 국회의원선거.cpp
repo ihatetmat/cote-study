76502fbfa6b3cd41d551dda2966b91b65de82b67
ihatetmat <kchk0628@naver.com>
2024-10-11 19:31:32 +0900

    go

--- 국회의원선거.cpp
+++ 국회의원선거.cpp
@@ -1,12 +1,42 @@
 #include <iostream>
-#include <queue>
+#include <vector>
 
 using namespace std;
 
 int main() {
+    int result = 0;
+    int n;
+    int king;
+
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
+    cin >> n;
+    cin >> king;
 
+    vector<int> v(n - 1);
+
+    for (int i = 0; i < n - 1; i++) {
+        cin >> v[i];
+    }
+
+    while (true) {
+        int max = 0;
+        int index = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (max < v[i]) {
+                max = v[i];
+                index = i;
+            }
+        }
+        if (king > max) {
+            cout << result << "\n";
+            break;
+        }
+        king++;
+        v[index]--;
+        result++;
+    }
 }
